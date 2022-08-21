7a8f65c8b990d879a0eaa13b3dbb1d5e2ab58273
Mike Roe <odinsgrudge@gmail.com>
2022-08-20 21:33:05 -0400

    Initial commit of python asyncio experiment

---
+++ ApiPoller.py
@@ -0,0 +1,56 @@
+import requests
+import asyncio
+import aiohttp
+
+class ApiPoller:
+    def __init__(self):
+        self.url = 'https://httpbin.org/'
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+
+    def process(self):
+        pass
+
+    def get_json(self):
+        endpoint = 'json'
+        self.call_api(self.url + endpoint)
+        pass
+
+    def get_json_loop(self):
+        endpoint = 'json'
+        self.call_api_with_loop(self.url + endpoint)
+
+    def get_json_async(self):
+        endpoint = 'json'
+        loop = asyncio.get_event_loop()
+        result = loop.run_until_complete(self.call_api_async(self.url + endpoint))
+
+    def run_delay(self, delay):
+        endpoint = f'delay/{delay}'
+        self.call_api(self.url + endpoint)
+
+    def call_api(self, url):
+        requests.request('GET', url, params=None, headers=self.headers)
+        
+    def call_api_with_loop(self, url):
+        print('Starting Loop')
+        for _ in range(1, 101):
+            requests.request('GET', url, params=None, headers=self.headers)
+        print('Exit loop')
+    
+    async def call_api_async(self, url):
+        print('Entering async')
+
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            for _ in range(1, 101):
+                tasks.append(session.get(url, headers=self.headers))
+            
+            responses = await asyncio.gather(*tasks)
+            for response in responses:
+                print(response)
+
+        print('Exiting async')
+
+
---
+++ main.py
@@ -0,0 +1,32 @@
+from ApiPoller import ApiPoller
+from time import perf_counter
+
+
+def json_loop(poller):
+    start_time = perf_counter()
+    poller.get_json_loop()
+    end_time = perf_counter()
+    print(f'JSON LOOP - Run time {end_time - start_time}')
+
+
+def json_async(poller):
+    start_time = perf_counter()
+    poller.get_json_async()
+    end_time = perf_counter()
+    print(f'JSON ASYNC - Run time {end_time - start_time}')
+
+
+def json_normal(poller):
+    start_time = perf_counter()
+    poller.get_json()
+    end_time = perf_counter()
+    print(f'JSON NORMAL - Run time {end_time - start_time}')
+
+if __name__ == '__main__':
+    api_poller = ApiPoller()
+    #json_normal(api_poller)
+    #json_loop(api_poller)
+    json_async(api_poller)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
